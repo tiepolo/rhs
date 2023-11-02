7d9895beed8664c1f99dd7faa928793d50d11629
tiepolo <tiepolo@sbcglobal.net>
2023-11-02 10:04:42 -0700

    Updated for new file

---
+++ date_extract.py
@@ -0,0 +1,38 @@
+import csv
+import datetime
+
+# Define the input and output file names
+input_file = 'membership.csv'
+output_file = 'membership_parsed.csv'
+
+# Define a function to convert timestamp to yyyy-mm-dd format
+def convert_timestamp(timestamp):
+    try:
+        timestamp_parts = timestamp.split()
+        if len(timestamp_parts) > 0:
+            date_obj = datetime.datetime.strptime(timestamp_parts[0], '%Y-%m-%d')
+            return date_obj.strftime('%Y-%m-%d')
+        else:
+            return None
+    except ValueError:
+        return None  # Return None for timestamps that cannot be parsed
+
+# Open the input CSV file and create an output CSV file
+with open(input_file, 'r') as csv_in, open(output_file, 'w', newline='') as csv_out:
+    reader = csv.DictReader(csv_in)
+    fieldnames = reader.fieldnames
+
+    # Create a writer for the output CSV file
+    writer = csv.DictWriter(csv_out, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for row in reader:
+        # Convert 'start_at', 'end_at', and 'membership_end_at' columns to yyyy-mm-dd format
+        row['start_at'] = convert_timestamp(row['start_at'])
+        row['end_at'] = convert_timestamp(row['end_at'])
+        row['membership_end_at'] = convert_timestamp(row['membership_end_at'])
+
+        # Write the modified row to the output CSV file
+        writer.writerow(row)
+
+print(f'Conversion complete. Output saved to {output_file}')
